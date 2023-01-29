53205da84f216b12562dc8e1969f9fb292140371
williamjiamin <33894470+williamjiamin@users.noreply.github.com>
2023-01-29 10:13:40 +0800

    Initial Commit

---
+++ QuestionBank/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuestionBank
+//
+//  Created by william on 2023/1/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
